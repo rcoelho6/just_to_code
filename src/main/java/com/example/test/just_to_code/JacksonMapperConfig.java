c2fc3027fa84a213f528d1b4e7f66f36ba025572
Rafael Coelho <rcoelho_6@hotmail.com>
2026-07-10 01:10:39 -0300

    test: use mapper builder customizer

--- src/main/java/com/example/test/just_to_code/JacksonMapperConfig.java
+++ src/main/java/com/example/test/just_to_code/JacksonMapperConfig.java
@@ -1,20 +1,33 @@
 package com.example.test.just_to_code;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.cfg.ConstructorDetector;
+import jakarta.annotation.Priority;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
 @Configuration
 public class JacksonMapperConfig {
-    
+
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer jackson2ObjectMapperBuilderCustomizer() {
+        return builder -> {
+            builder.createXmlMapper(false)
+                    .failOnUnknownProperties(true)
+                    .featuresToEnable(DeserializationFeature.FAIL_ON_MISSING_CREATOR_PROPERTIES)
+                    .postConfigurer(objectMapper ->
+                            objectMapper.setConstructorDetector(ConstructorDetector.USE_PROPERTIES_BASED)
+                    );;
+        };
+    }
+
 //    @Bean
 //    @Primary
 //    public ObjectMapper objectMapper(Jackson2ObjectMapperBuilder builder) {
 //        return builder.createXmlMapper(false)
 //                .failOnUnknownProperties(false)
-//                .featuresToDisable(DeserializationFeature.FAIL_ON_MISSING_CREATOR_PROPERTIES)
+//                .featuresToEnable(DeserializationFeature.FAIL_ON_MISSING_CREATOR_PROPERTIES)
 //                .build();
 //    }
 }
